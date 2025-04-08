7f0c9a7cdbefc468c3f8b169b61c8cc0a6b91468
LeeWonGun <wongun1850@gmail.com>
2025-04-08 14:22:36 +0900

    Update urls.py(랜덤 상식 3개 리턴 API 경로 추가)

--- myservice/urls.py
+++ myservice/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from myapp import views
+from .views import get_random_explanations
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('find-id/', views.FindIdView.as_view(), name='findid'),
     path('reset-password/', views.ResetPasswordView.as_view(), name='resetpassword')
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
+    path('api/questions/random_explanations/', get_random_explanations),
 ]
