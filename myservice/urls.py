a2b50e3b525efc795cc3829af71e3d2bdb0f7cc6
minjunsong <songmj5000@naver.com>
2025-04-08 19:07:52 +0900

    04/08 오류 수정

--- myservice/urls.py
+++ myservice/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('find-id/', views.FindIdView.as_view(), name='findid'),
-    path('reset-password/', views.ResetPasswordView.as_view(), name='resetpassword')
+    path('reset-password/', views.ResetPasswordView.as_view(), name='resetpassword'),
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('api/questions/random_explanations/', get_random_explanations),
     path('daily-facts/', get_daily_facts),
