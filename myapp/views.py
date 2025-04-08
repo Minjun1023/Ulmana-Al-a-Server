a5a4f3dcff08ae629f9d46fb47314d26e2f8ed89
LeeWonGun <wongun1850@gmail.com>
2025-04-08 14:42:28 +0900

    Update views.py(데일리 상식 반환 API 추가)

--- myapp/views.py
+++ myapp/views.py
@@ -11,6 +11,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from .models import Question  # ← 문제 모델도 import 필요
 import random
@@ -98,3 +100,25 @@ def get_random_explanations(request):
     explanations = [q.explanation for q in random.sample(list(questions), min(3, len(questions)))]
 
     return Response({"explanations": explanations})
+
+
+@csrf_exempt
+def get_daily_facts(request):
+    email = request.GET.get('email')  # 안드로이드에서 보낼 예정
+
+    try:
+        user = CustomUser.objects.get(email=email)
+        genre_ids = []
+
+        for interest in [user.interest1, user.interest2, user.interest3]:
+            if interest:
+                genre_ids.append(int(interest))
+
+        questions = Question.objects.filter(genre_id__in=genre_ids)
+        explanations = list(questions.values_list('explanation', flat=True))
+        random.shuffle(explanations)
+
+        return JsonResponse({'daily_facts': explanations[:3]}, status=200)
+
+    except CustomUser.DoesNotExist:
+        return JsonResponse({'error': 'User not found'}, status=404)
