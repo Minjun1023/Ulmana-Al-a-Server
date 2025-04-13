396e0b560bb9915ac21ccd65e9a212fa4704736b
minjunsong <songmj5000@naver.com>
2025-04-14 01:52:42 +0900

    04/14 csv -> json 파일로 변경 및 문제 개수 증가

--- myapp/management/commands/import_questions.py
+++ myapp/management/commands/import_questions.py
@@ -1,21 +1,21 @@
-import csv
+import json
 from django.core.management.base import BaseCommand
 from myapp.models import Question, Genre
 
 class Command(BaseCommand):
-    help = 'CSV 파일에서 상식 문제를 가져와 DB에 저장합니다.'
+    help = 'JSON 파일에서 상식 문제를 가져와 DB에 저장합니다.'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_path', type=str, help='CSV 파일 경로')
+        parser.add_argument('json_path', type=str, help='JSON 파일 경로')
 
     def handle(self, *args, **options):
-        path = options['csv_path']
+        path = options['json_path']
         inserted_count = 0
         skipped_count = 0
 
-        with open(path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
+        with open(path, encoding='utf-8') as jsonfile:
+            data = json.load(jsonfile)
+            for row in data:
                 try:
                     genre = Genre.objects.get(genre_id=row['genre_id'])
                 except Genre.DoesNotExist:
