32db9571822fa261b1884139e30eea57dc66ec16
Minjun1023 <157129896+Minjun1023@users.noreply.github.com>
2025-04-18 00:17:50 +0900

    퀴즈 제출 추가

--- myservice/urls.py
+++ myservice/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from myapp import views
 from myapp.views import get_random_explanations
 from myapp.views import get_daily_facts
+from myapp.views import Genre25QuestionView
+from myapp.views import Genre50QuestionView
+from myapp.views import SpeedQuizView
+from myapp.views import QuizSubmitView
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
@@ -11,5 +15,8 @@ urlpatterns = [
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('api/questions/random_explanations/', get_random_explanations),
     path('daily-facts/', views.get_daily_facts),
-
+    path('questions/genre/25/', Genre25QuestionView.as_view(), name='genre_25_questions'), 
+    path('questions/genre/50/', Genre50QuestionView.as_view(), name='genre_50_questions'), 
+    path('questions/speed/', SpeedQuizView.as_view(), name='speed_quiz'),
+    path('quiz/submit/', QuizSubmitView.as_view(), name='quiz_submit'),  
 ]
